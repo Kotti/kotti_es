88e96275956ba1a29e7a252eb35d74b89a3ca663
davidemoro <davide.moro@gmail.com>
2015-04-15 10:08:15 +0200

    implemented Kotti specific adapter

--- kotti_es/elastic.py
+++ kotti_es/elastic.py
@@ -1,6 +1,3 @@
-from zope.interface import (
-    implements,
-    )
 from zope.component import (
     adapts,
     )
@@ -10,35 +7,11 @@ from kotti.interfaces import (
 from pyramid_es.mixin import (
     ESMapping,
     ESString,
-    ElasticParent,
     )
-from .interfaces import IElastic
-
-
-class ElasticBase(object):
-
-    def __init__(self, context):
-        self.context = context
-
-    @classmethod
-    def elastic_mapping(cls):
-        """
-        Return an ES mapping.
-        """
-        raise NotImplementedError("ES classes must define a mapping")
-
-    def elastic_document(self):
-        """
-        Apply the class ES mapping to the current context
-        """
-        return self.elastic_mapping()(self)
-
-    __elastic_parent__ = None
-    elastic_parent = ElasticParent()
+from pyramid_es.elastic import ElasticBase
 
 
 class ElasticContent(ElasticBase):
-    implements(IElastic)
     adapts(IContent)
 
     @classmethod
@@ -52,3 +25,9 @@ class ElasticContent(ElasticBase):
             analyzer='content',
             properties=ESMapping(
                 ESString('description', boost=5.0)))
+
+    def elastic_document_type(self):
+        """
+        The elastic document type
+        """
+        return self.context.type_info.name
