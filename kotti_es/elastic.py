00fb4036caa46f536cec1550829498450c0ef424
davidemoro <davide.moro@gmail.com>
2015-04-20 15:01:59 +0200

    implemented first version of kotti base ES generic indexer

--- kotti_es/elastic.py
+++ kotti_es/elastic.py
@@ -8,29 +8,71 @@ from kotti.interfaces import (
 from pyramid_es.mixin import (
     ESMapping,
     ESString,
+    ESField,
     )
 from pyramid_es.elastic import ElasticBase
 from pyramid_es.interfaces import IElastic
 
 
-class ElasticContent(ElasticBase):
+class BaseElasticKottiContent(ElasticBase):
     implements(IElastic)
     adapts(IContent)
 
-    @classmethod
-    def elastic_mapping(cls):
+    def elastic_mapping(self):
         """
-        Return an ES mapping.
+        This is the base fallback adapter for all contents.
+
+        If exists an ``elastic_mapping`` configuration is available you the
+        resource's ``type_info``, it will returned as it is.
+
+        Otherwise, a very basic default mapping is returned (just ``title`` and
+        ``description``).
+
+        You can add a ``filter`` keyword argument to the ``ESString``
+        properties passing a callable that accepts the value to be
+        filtered/changed. For example you could pass a filter=html_to_text
+        to a ``body`` property.
         """
-        # TODO: return all mappings dynamically
-        return ESMapping(
-            attr='context',
-            analyzer='content',
-            properties=ESMapping(
-                ESString('description', boost=5.0)))
+        context = self.context
+        elastic_mapping = getattr(context.type_info, 'elastic_mapping', None)
+        if elastic_mapping is not None:
+            return elastic_mapping
+        else:
+            return ESMapping(
+                analyzer='content',
+                properties=ESMapping(
+                    ESField('_id'),
+                    ESString('title', attr='_title'),
+                    ESString('description', attr='_description'),
+                    ESString('body', attr='_body'),
+                    ))
 
     def elastic_document_type(self):
         """
         The elastic document type
         """
         return self.context.type_info.name
+
+    def elastic_document_id(self):
+        """
+        The elastic document id
+        """
+        return self.context.id
+
+    # Attr private methods
+    @property
+    def _id(self):
+        return self.elastic_document_id()
+
+    @property
+    def _title(self):
+        return self.context.title
+
+    @property
+    def _description(self):
+        return self.context.title
+
+    @property
+    def _body(self):
+        body = getattr(self.context, 'body', '')
+        return body
