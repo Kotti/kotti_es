c69ce1d234698548a41298d342bdbd29d976c4d2
davidemoro <davide.moro@gmail.com>
2015-04-21 11:03:07 +0200

    fix wrong test setup

--- kotti_es/tests/conftest.py
+++ kotti_es/tests/conftest.py
@@ -12,8 +12,6 @@ pytest_plugins = "kotti"
 
 @fixture(scope='session')
 def custom_settings():
-    import kotti_es.resources
-    kotti_es.resources  # make pyflakes happy
     return {
         'kotti.configurators': 'kotti_tinymce.kotti_configure '
                                'kotti_es.kotti_configure'}
