942451c7af7e57501d9f7c165814c49f88449e97
davidemoro <davide.moro@gmail.com>
2015-04-21 15:53:21 +0200

    more test coverage

--- kotti_es/tests/test_events.py
+++ kotti_es/tests/test_events.py
@@ -9,3 +9,89 @@ class TestEvents:
         with mock.patch('kotti_es.events.sqlalchemy.event.listen') as listen:
             wire_sqlalchemy()
             assert listen.call_args_list == []
+
+    def test_after_insert(self, dummy_request):
+        from kotti.resources import Document
+        document = Document()
+        document.request = dummy_request
+        assert hasattr(document.request, '_index_list') is False
+        from kotti_es.events import _after_insert_update
+        from kotti_es.events import INSERT_CODE
+        _after_insert_update(None, None, document)
+        assert getattr(document.request, '_index_list') == [
+            (document, INSERT_CODE)
+            ]
+
+    def test_after_delete(self, dummy_request):
+        from kotti.resources import Document
+        document = Document()
+        document.request = dummy_request
+        assert hasattr(document.request, '_index_list') is False
+        from kotti_es.events import _after_delete
+        from kotti_es.events import DELETE_CODE
+        _after_delete(None, None, document)
+        assert getattr(document.request, '_index_list') == [
+            (document, DELETE_CODE)
+            ]
+
+    def test_after_delete2(self, dummy_request):
+        from kotti.resources import Document
+        document = Document()
+        document.request = dummy_request
+        document.request._index_list = []
+        from kotti_es.events import _after_delete
+        from kotti_es.events import DELETE_CODE
+        _after_delete(None, None, document)
+        assert getattr(document.request, '_index_list') == [
+            (document, DELETE_CODE)
+            ]
+
+    def test_after_delete3(self, dummy_request):
+        from kotti.resources import Document
+        document1 = Document()
+        document1.request = dummy_request
+        document2 = Document()
+        document2.request = dummy_request
+        from kotti_es.events import _after_delete
+        _after_delete(None, None, document1)
+        _after_delete(None, None, document2)
+        assert len(getattr(dummy_request, '_index_list')) == 2
+
+    def test_after_commit_insert(self, dummy_request):
+        from kotti.resources import Document
+        from kotti_es.events import _after_insert_update
+        from kotti_es.events import _after_commit
+        document = Document()
+        document.request = dummy_request
+        _after_insert_update(None, None, document)
+
+        import mock
+        with mock.patch('kotti_es.events.get_current_request') as \
+                get_current_request:
+            get_current_request.return_value = dummy_request
+            with mock.patch('kotti_es.events.get_client') as \
+                    get_client:
+                magic = mock.MagicMock()
+                get_client.return_value = magic
+                _after_commit(None)
+                magic.index_object.assert_called_with(document, immediate=True)
+
+    def test_after_commit_delete(self, dummy_request):
+        from kotti.resources import Document
+        from kotti_es.events import _after_delete
+        from kotti_es.events import _after_commit
+        document = Document()
+        document.request = dummy_request
+        _after_delete(None, None, document)
+
+        import mock
+        with mock.patch('kotti_es.events.get_current_request') as \
+                get_current_request:
+            get_current_request.return_value = dummy_request
+            with mock.patch('kotti_es.events.get_client') as \
+                    get_client:
+                magic = mock.MagicMock()
+                get_client.return_value = magic
+                _after_commit(None)
+                magic.delete_object.assert_called_with(document,
+                                                       immediate=True)
