2addd1c48bf79ce3ab30576c9bd20d0d25e1f095
davidemoro <davide.moro@gmail.com>
2015-04-21 11:03:21 +0200

    fix base test adapter

--- kotti_es/tests/test_elastic.py
+++ kotti_es/tests/test_elastic.py
@@ -14,3 +14,16 @@ class TestDefaultKottiAdapter:
         from zope.interface.verify import verifyObject
         adapter = BaseElasticKottiContent(None)
         assert verifyObject(IElastic, adapter)
+
+    def test_verify_document(self, root):
+        from kotti.resources import Document
+        from kotti_es.elastic import BaseElasticKottiContent
+        document = Document(title='mytitle',
+                            description='mydescr',
+                            body='<span>hello</span>')
+        root['document'] = document
+        assert root['document'].name == 'document'
+
+        adapter = BaseElasticKottiContent(document)
+        assert adapter.elastic_document_type() == document.type_info.name
+        assert adapter.elastic_document_id() == document.id
