9c8cee947bfff2fa507d73a13720e46c62d45a94
davidemoro <davide.moro@gmail.com>
2015-04-21 18:10:46 +0200

    fixed random failures with client.flush. Also wiped out ES index

--- kotti_es/tests/test_indexing.py
+++ kotti_es/tests/test_indexing.py
@@ -1,38 +1,68 @@
+from pytest import fixture
+
+
+@fixture
+def es_client(dummy_request, config):
+    config.include('kotti_es')
+    from pyramid_es import get_client
+    client = get_client(dummy_request)
+    client.es.indices.delete(index=client.index, ignore=[400, 404])
+    client.flush()
+    client.ensure_index(recreate=True)
+    client.flush()
+    return client
+
+
 class TestIndexing:
 
-    def test_insert(self, config, db_session, root, dummy_request):
+    def test_insert(self, es_client, root, dummy_request):
         import transaction
-        config.include('kotti_es')
         from kotti.resources import Document
-        document = Document(title=u'mydoc', description=u'description',)
+        document = Document(title=u'mydoc', description=u'mydescription',)
         document.request = dummy_request
         root[u'mydoc'] = document
         # with body=None no exceptions should be raised (utils/html_to_text)
         transaction.commit()
-        from pyramid_es import get_client
-        client = get_client(dummy_request)
 
-        results = client.es.search(q='description')
+        es_client.flush()
+        results = es_client.es.search(q='mydescription')
         _type = results['hits']['hits'][0]['_type']
         assert _type == 'Document'
         assert results['hits']['hits'][0]['_source']['path'] == '/mydoc'
 
-    def test_insert_searches(self, config, root, dummy_request):
+    def test_insert_searches(self, es_client, root,
+                             dummy_request):
         import transaction
-        config.include('kotti_es')
         from kotti.resources import Document
-        document = Document(title=u'title', description=u'description',
-                            body=u'<span>body</span>')
+        document = Document(title=u'mytitle', description=u'mydescription',
+                            body=u'<span>mybody</span>')
         document.request = dummy_request
         root[u'mydoc'] = document
         # with body=None no exceptions should be raised (utils/html_to_text)
         transaction.commit()
-        from pyramid_es import get_client
-        client = get_client(dummy_request)
 
-        assert len(client.es.search(q='title')['hits']['hits']) == 1
-        assert len(client.es.search(q='description')['hits']['hits']) == 1
-        # TODO: test failure, ok if you put a pdb
-        # assert len(client.es.search(q='body')['hits']['hits']) == 1
-        assert len(client.es.search(q='mydoc')['hits']['hits']) == 1
-        assert len(client.es.search(q='span')['hits']['hits']) == 0
+        es_client.flush()
+        assert len(es_client.es.search(q='mytitle')['hits']['hits']) == 1
+        assert len(es_client.es.search(q='mydescription')['hits']['hits']) == 1
+        assert len(es_client.es.search(q='mybody')['hits']['hits']) == 1
+        assert len(es_client.es.search(q='mydoc')['hits']['hits']) == 1
+        assert len(es_client.es.search(q='span')['hits']['hits']) == 0
+
+    def test_insert_delete(self, es_client, root,
+                           dummy_request):
+        import transaction
+        from kotti.resources import Document
+        document = Document(title=u'mytitle', description=u'mydescription',
+                            body=u'<span>mybody</span>')
+        document.request = dummy_request
+        root[u'mydoc'] = document
+        # with body=None no exceptions should be raised (utils/html_to_text)
+        transaction.commit()
+
+        es_client.flush()
+        assert len(es_client.es.search(q='mytitle')['hits']['hits']) == 1
+
+#        del root[u'mydoc']
+#        transaction.commit()
+#         es_client.flush()
+#        assert len(es_client.es.search(q='title')['hits']['hits']) == 0
