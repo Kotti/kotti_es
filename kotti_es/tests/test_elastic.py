8fd1946c362c3ff33688d0b2582e471c56c22a29
davidemoro <davide.moro@gmail.com>
2015-04-21 11:48:05 +0200

    added more adapter tests

--- kotti_es/tests/test_elastic.py
+++ kotti_es/tests/test_elastic.py
@@ -27,3 +27,43 @@ class TestDefaultKottiAdapter:
         adapter = BaseElasticKottiContent(document)
         assert adapter.elastic_document_type() == document.type_info.name
         assert adapter.elastic_document_id() == document.id
+
+    def test_verify_document_default_mapping(self, root):
+        from kotti.resources import Document
+        from kotti_es.elastic import BaseElasticKottiContent
+        document = Document(title='mytitle',
+                            description='mydescr',
+                            body='<span>hello</span>')
+        root['document'] = document
+        assert root['document'].name == 'document'
+
+        adapter = BaseElasticKottiContent(document)
+        elastic_mapping = adapter.elastic_mapping()
+        es_mapping_props = elastic_mapping.properties
+        assert 'title' in es_mapping_props
+        assert 'description' in es_mapping_props
+        assert 'body' in es_mapping_props
+        assert 'path' in es_mapping_props
+        assert '_id' in es_mapping_props
+        assert 'name' in es_mapping_props
+
+    def test_verify_document_default_mapping_attrs(self, root):
+        from kotti.resources import Document
+        from kotti_es.elastic import BaseElasticKottiContent
+        document = Document(title='mytitle',
+                            description='mydescr',
+                            body='<span>hello</span>')
+        root['document'] = document
+        assert root['document'].name == 'document'
+
+        adapter = BaseElasticKottiContent(document)
+        elastic_mapping = adapter.elastic_mapping()
+        es_mapping_props = elastic_mapping.properties
+        assert es_mapping_props['title'].attr == '_title'
+        assert es_mapping_props['description'].attr == '_description'
+        assert es_mapping_props['body'].attr == '_body'
+        from kotti_es.util import html_to_text
+        assert es_mapping_props['body'].filter == html_to_text
+        assert es_mapping_props['path'].attr == '_path'
+        assert es_mapping_props['_id'].attr is None
+        assert es_mapping_props['name'].attr == '_name'
