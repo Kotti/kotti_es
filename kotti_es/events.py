9e40b26943a8b9e863316ac34584d793f6d4a377
davidemoro <davide.moro@gmail.com>
2015-04-23 15:31:22 +0200

    fix issue #9 (code review please)

--- kotti_es/events.py
+++ kotti_es/events.py
@@ -29,48 +29,67 @@ def _after_insert_update(mapper, connection, target):
     if not is_blacklisted(target):
         request = get_request(target)
         if request:
+            target_id = target.id
             if not hasattr(request, '_index_list'):
-                request._index_list = [(target, INSERT_CODE)]
+                request._index_list = [(target_id, INSERT_CODE)]
             else:
-                request._index_list.append((target, INSERT_CODE))
+                request._index_list.append((target_id, INSERT_CODE))
 
 
 def _after_delete(mapper, connection, target):
     if not is_blacklisted(target):
         request = get_request(target)
         if request:
+            target_id = target.id
             if not hasattr(request, '_index_list'):
-                request._index_list = [(target, DELETE_CODE)]
+                request._index_list = [(target_id, DELETE_CODE)]
             else:
-                request._index_list.append((target, DELETE_CODE))
+                request._index_list.append((target_id, DELETE_CODE))
 
 
 def _after_commit(session):
     request = get_current_request()
     if request:
-        index_list = getattr(request, '_index_list', [])
-        es_client = get_client(request)
-        for target, operation in index_list:
-            target_id = target.id
+        default_index_action(session, request)
+
+
+def default_index_action(session, request):
+    def get_target_by_id(session, target_id):
+        return session.query(Content).filter_by(id=target_id).one()
+
+    def update_target(session, es_client, target, operation):
+        target = session.query(Content).filter_by(id=target.id).one()
+        if operation == INSERT_CODE:
+            es_client.index_object(target, immediate=True)
+        elif operation == DELETE_CODE:
             try:
-                target.name
+                es_client.delete_object(target, immediate=True)
+            except NotFoundError:
+                pass
+    request = get_current_request()
+    if request:
+        index_list = getattr(request, '_index_list', [])
+        if index_list:
+            new_session = False
+            es_client = get_client(request)
+            target_id = index_list[0][0]
+            operation = index_list[0][1]
+            try:
+                target = get_target_by_id(session, target_id)
             except sqlalchemy.exc.InvalidRequestError:
                 # if you call the @@rename view and you
                 # try to change name, you'll get an error
                 # because the session is no more usable.
-                # Possible improvements:
-                # * find another way to detect if the session is usable or not
-                # * do not initialize a new session for each cycle items
-                # TODO: code review and refactor needed
+                # Code review will be very appreciated
+                new_session = True
                 session = scoped_session(sessionmaker())
-                target = session.query(Content).filter_by(id=target_id).first()
-            if operation == INSERT_CODE:
-                es_client.index_object(target, immediate=True)
-            elif operation == DELETE_CODE:
-                try:
-                    es_client.delete_object(target, immediate=True)
-                except NotFoundError:
-                    pass
+                target = get_target_by_id(session, target_id)
+            update_target(session, es_client, target, operation)
+            for target_id, operation in index_list[1:]:
+                target = get_target_by_id(session, target_id)
+                update_target(session, es_client, target, operation)
+            if new_session:
+                session.close()
 
 
 def wire_sqlalchemy():  # pragma: no cover
