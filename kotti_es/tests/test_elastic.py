247ee11d5cb46c148fffe291de2fa159fc70b7eb
davidemoro <davide.moro@gmail.com>
2015-06-11 10:06:32 +0200

    fix wrong test (no more adapts None)

--- kotti_es/tests/test_elastic.py
+++ kotti_es/tests/test_elastic.py
@@ -5,14 +5,16 @@ class TestDefaultKottiAdapter:
     def test_kotti_adapter_iface(self):
         from pyramid_es.interfaces import IElastic
         from kotti_es.elastic import BaseElasticKottiContent
-        adapter = BaseElasticKottiContent(None)
+        from kotti.resources import Document
+        adapter = BaseElasticKottiContent(Document())
         assert IElastic.providedBy(adapter)
 
     def test_verify_adapter(self):
         from pyramid_es.interfaces import IElastic
         from kotti_es.elastic import BaseElasticKottiContent
         from zope.interface.verify import verifyObject
-        adapter = BaseElasticKottiContent(None)
+        from kotti.resources import Document
+        adapter = BaseElasticKottiContent(Document())
         assert verifyObject(IElastic, adapter)
 
     def test_verify_document(self, root):
